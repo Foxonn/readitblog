1a35bf35b4d82c681f956a0ad32d06d31490ae5c
Foxonn <psihonort@gmail.com>
2020-06-30 14:44:57 +0600

    change allow_host *

--- webapp/readit/readit/_settings/__init__.py
+++ webapp/readit/readit/_settings/__init__.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['readit.loc', '127.0.0.1', '127.0.0.1:5000']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
